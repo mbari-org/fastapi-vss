4757fe8db7c20caaadb845f6b1837737c586e002
Danelle Cline <dcline@mbari.org>
2024-08-12 15:53:34 -0700

    chore: improved cuda build

--- bin/run_prod.sh
+++ bin/run_prod.sh
@@ -5,18 +5,13 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE_DIR="$(cd "$(dirname "${SCRIPT_DIR}/../.." )" && pwd )"
 
-# Export the environment variables in the .env file
-export $(grep -v '^#' $BASE_DIR/.env |  xargs)
-
-# Get the short version of the hash of the commit
-git_hash=$(git log -1 --format=%h)
-
 # Get the user and group id
 MLDEVOPS_UID=$(id -u)
 MLDEVOPS_GID=$(id -g)
 
-echo $MLDEVOPS_UID > .env
-echo $MLDEVOPS_GID >> .env
+echo MLDEVOPS_UID=$MLDEVOPS_UID >> .env
+echo MLDEVOPS_GID=$MLDEVOPS_GID >> .env
 
 # Run the production compose stack with the MLDEVOPS_UID and MLDEVOPS_GID
-GIT_VERSION="${git_hash}" COMPOSE_PROJECT_NAME=fastapi-vss docker-compose -f compose.yml up -d --build --force-recreate --runtime nvidia --gpus all --no-deps -
+#GIT_VERSION="${git_hash}" COMPOSE_PROJECT_NAME=fastapi-vss docker-compose -f compose.yml up -d --build --force-recreate --runtime nvidia --gpus all --no-deps -
+echo COMPOSE_PROJECT_NAME=fastapi-vss docker-compose -f compose.yml up -d --build --force-recreate --runtime nvidia --gpus all --no-deps .
