702866eef7d4e1fd4aa118c7d534792a1e577087
danellecline <dcline@mbari.org>
2024-08-12 16:24:06 -0700

    chore: more docker clean-up

--- bin/run_prod.sh
+++ bin/run_prod.sh
@@ -5,13 +5,11 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE_DIR="$(cd "$(dirname "${SCRIPT_DIR}/../.." )" && pwd )"
 
-# Get the user and group id
-MLDEVOPS_UID=$(id -u)
-MLDEVOPS_GID=$(id -g)
+# Get the short version of the hash of the commit
+git_hash=$(git log -1 --format=%h)
 
-echo MLDEVOPS_UID=$MLDEVOPS_UID >> .env
-echo MLDEVOPS_GID=$MLDEVOPS_GID >> .env
+# Run the production compose stack
+GIT_VERSION="${git_hash}"
 
 # Run the production compose stack with the MLDEVOPS_UID and MLDEVOPS_GID
-#GIT_VERSION="${git_hash}" COMPOSE_PROJECT_NAME=fastapi-vss docker-compose -f compose.yml up -d --build --force-recreate --runtime nvidia --gpus all --no-deps -
-echo COMPOSE_PROJECT_NAME=fastapi-vss docker-compose -f compose.yml up -d --build --force-recreate --runtime nvidia --gpus all --no-deps .
+GIT_VERSION="${git_hash}" COMPOSE_PROJECT_NAME=fastapi-vss docker-compose -f ${BASE_DIR}/docker/compose.yml up -d --build --force-recreate --runtime nvidia --gpus all --no-deps .
